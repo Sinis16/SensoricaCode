b5c9b413237285c4561b6183163c915e178ce3b0
JuanPuig10 <123596813+JuanPuig10@users.noreply.github.com>
2024-02-20 17:08:19 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,9 +2,9 @@ import serial
 
 if __name__ == '__main__': 
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) 
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) 
 
-ser.reset_input_buffer() 
+  ser.reset_input_buffer() 
 
 while True: 
 
