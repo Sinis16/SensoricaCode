4e04cf5176261d248b72ba1e0fc4c3f0637fb664
Sinis16 <s.sinisterra@uniandes.edu.co>
2024-02-22 02:23:10 -0500

    b

--- pruebaSensorica.py
+++ pruebaSensorica.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
         number = ser.read()
         print(number)
         if number != b'0':
-            if int.from_bytes(number, byteorder='big') == 18:
-                led_number = random.randint(1,4)
-                print("Button has been pressed.")
-                print("Sending number " + str(led_number) + " to Arduino.")
-                ser.write(str(led_number).encode('utf-8'))
+            led_number = random.randint(1,4)
+            print("Button has been pressed.")
+            print("Sending number " + str(led_number) + " to Arduino.")
+            ser.write(str(led_number).encode('utf-8'))
